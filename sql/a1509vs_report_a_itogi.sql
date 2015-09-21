d137a43b8a26ce5d6d8cee1afa679a042bdca5e7
Denis Yakovenko <maximka1945@gmail.com>
2015-09-21 18:19:40 +0300

    21/09/2015 18:19

---
+++ sql/a1509vs_report_a_itogi.sql
@@ -0,0 +1,57 @@
+/* Formatted on 21/09/2015 17:44:02 (QP5 v5.227.12220.39724) */
+SELECT COUNT (DISTINCT an.H_TP_KOD_DATA_NAKL) cnt_nakl,
+       COUNT (DISTINCT tp.tp_kod) cnt_tp,
+       SUM (d.nakl_summ) nakl_summ,
+       SUM (d.act_summ) act_summ,
+       SUM (DECODE (an.bonus_dt1, NULL, NULL, an.bonus_sum1)) bonus_sum,
+       SUM (DECODE (an.bonus_dt1, NULL, NULL, 1)) bonus_dt_cnt,
+       SUM ( (SELECT DECODE (lu, NULL, 0, 1)
+                FROM ACT_OK
+               WHERE     tn = (SELECT parent
+                                 FROM parents
+                                WHERE tn = st.tn)
+                     AND m = :month
+                     AND act = :act))
+          ok_chief,
+         DECODE (NVL (SUM (d.nakl_summ), 0),
+                 0, 0,
+                 (SUM (an.bonus_sum1)) / SUM (d.nakl_summ))
+       * 100
+          zat
+  FROM a1509vs d,
+       a1509vs_action_nakl an,
+       user_list st,
+       a1509vs_tp_select tp
+ WHERE     d.tab_num = st.tab_num
+       AND st.tn IN
+              (SELECT slave
+                 FROM full
+                WHERE master =
+                         DECODE (:exp_list_without_ts,
+                                 0, master,
+                                 :exp_list_without_ts))
+       AND st.tn IN
+              (SELECT slave
+                 FROM full
+                WHERE master =
+                         DECODE (:exp_list_only_ts,
+                                 0, master,
+                                 :exp_list_only_ts))
+       AND (   st.tn IN (SELECT slave
+                           FROM full
+                          WHERE master = DECODE (:tn, -1, master, :tn))
+            OR (SELECT NVL (is_traid, 0)
+                  FROM user_list
+                 WHERE tn = :tn) = 1)
+       AND st.dpt_id = :dpt_id
+       AND d.H_TP_KOD_DATA_NAKL = an.H_TP_KOD_DATA_NAKL
+       AND d.tp_kod = tp.tp_kod
+       AND DECODE (:eta_list, '', d.h_fio_eta, :eta_list) = d.h_fio_eta
+       AND an.if1 = 1
+       AND DECODE (:ok_bonus,  1, 0,  2, 1,  3, 0) =
+              DECODE (:ok_bonus,
+                      1, 0,
+                      2, DECODE (an.bonus_dt1, NULL, 0, 1),
+                      3, DECODE (an.bonus_dt1, NULL, 0, 1))
+       AND TO_NUMBER (TO_CHAR (d.data, 'mm')) = :month
+       AND tp.m = :month
---
+++ sql/a1509vs_tp_select_total.sql
@@ -0,0 +1,39 @@
+/* Formatted on 21/09/2015 13:24:25 (QP5 v5.227.12220.39724) */
+SELECT SUM (selected)
+  FROM (  SELECT a.tab_num,
+                 st.fio fio_ts,
+                 a.fio_eta,
+                 a.tp_ur,
+                 a.tp_addr,
+                 a.tp_kod,
+                 DECODE (tp.tp_kod, NULL, NULL, 1) selected,
+                 NVL (tp.contact_lpr,
+                      (SELECT TRIM (contact_tel || ' ' || contact_fio)
+                         FROM routes
+                        WHERE tp_kod = a.tp_kod AND ROWNUM = 1))
+                    contact_lpr
+            FROM a1509vs_tp_select tp,
+                 a1509vs a,
+                 user_list st,
+                 a1509vs_flag f
+           WHERE     a.tab_num = st.tab_num
+                 AND (   st.tn IN (SELECT slave
+                                     FROM full
+                                    WHERE master = :tn)
+                      OR (SELECT NVL (is_traid, 0)
+                            FROM user_list
+                           WHERE tn = :tn) = 1)
+                 AND a.tp_kod = tp.tp_kod(+)
+                 AND st.dpt_id = :dpt_id
+                 AND a.tp_kod = f.tp_kod(+) and f.tp_kod is null
+                 AND TO_NUMBER (TO_CHAR (a.data, 'mm')) = :month
+                 AND tp.m(+) = :month
+        GROUP BY a.tab_num,
+                 st.fio,
+                 a.fio_eta,
+                 a.tp_ur,
+                 a.tp_addr,
+                 a.tp_kod,
+                 tp.contact_lpr,
+                 DECODE (tp.tp_kod, NULL, NULL, 1)
+        ORDER BY st.fio, a.fio_eta, a.tp_ur)
