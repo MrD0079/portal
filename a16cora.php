424433ba424ce3a27a2e53d8ba975c1d7a2cb246
Denis Yakovenko <maximka1945@gmail.com>
2016-12-13 18:03:27 +0200

    new file:   a16cora.php 	modified:   nbproject/private/private.xml 	new file:   sql/a16cora.sql 	new file:   sql/a16cora_photos.sql 	new file:   tpl/a16cora.html 	modified:   tpl/menu_territory.html

---
+++ a16cora.php
@@ -0,0 +1,85 @@
+<?php
+if (isset($_REQUEST["save"]))
+{
+        $_REQUEST = recursive_iconv ('UTF-8', 'Windows-1251', $_REQUEST);
+	$keys = array('visitdate'=>OraDate2MDBDate($_REQUEST['visitdate']),'tp_kod'=>$_REQUEST['tp_kod']);
+	if ($_REQUEST['field1']=='undefined')
+        {
+            $vals = array($_REQUEST['field']=>$_REQUEST['val']);
+        }
+        else
+        {
+            $vals = array($_REQUEST['field']=>$_REQUEST['val'],$_REQUEST['field1']=>$_REQUEST['val1']);
+        }
+	Table_Update('a16cotp', $keys,$vals);
+}
+else if (isset($_REQUEST["saveurl"]))
+{
+        $_REQUEST = recursive_iconv ('UTF-8', 'Windows-1251', $_REQUEST);
+	$keys = array('h_url'=>$_REQUEST['hurl']);
+	if ($_REQUEST['val']==1)
+        {
+            $vals = $keys;
+        }
+        else
+        {
+            $vals = null;
+        }
+	Table_Update('a16courl', $keys,$vals);
+}
+else
+{
+	InitRequestVar("exp_list_without_ts",0);
+	InitRequestVar("exp_list_only_ts",0);
+	InitRequestVar("eta_list",$_SESSION["h_eta"]);
+	InitRequestVar("sd",$now);
+	InitRequestVar("ed",$now);
+	InitRequestVar("ok_ts",1);
+	InitRequestVar("ok_tm",1);
+	InitRequestVar("st_ts",1);
+	InitRequestVar("st_tm",1);
+	InitRequestVar("dt",$now);
+	$params=array(
+		':dpt_id' => $_SESSION["dpt_id"],
+		':tn'=>$tn,
+		':ok_ts' => $_REQUEST["ok_ts"],
+		':ok_tm' => $_REQUEST["ok_tm"],
+		':st_ts' => $_REQUEST["st_ts"],
+		':st_tm' => $_REQUEST["st_tm"],
+		':eta_list' => "'".$_REQUEST["eta_list"]."'",
+		':sd' => "'".$_REQUEST["sd"]."'",
+		':ed' => "'".$_REQUEST["ed"]."'",
+		':exp_list_without_ts' => $_REQUEST["exp_list_without_ts"],
+		':exp_list_only_ts' => $_REQUEST["exp_list_only_ts"],
+	);
+	$sql = rtrim(file_get_contents('sql/exp_list_from_parent_only_ts.sql'));
+	$sql=stritr($sql,$params);
+	$exp_list_only_ts = $db->getAll($sql, null, null, null, MDB2_FETCHMODE_ASSOC);
+	$smarty->assign('exp_list_only_ts', $exp_list_only_ts);
+	$sql = rtrim(file_get_contents('sql/exp_list_from_parent_without_ts.sql'));
+	$sql=stritr($sql,$params);
+	$exp_list_without_ts = $db->getAll($sql, null, null, null, MDB2_FETCHMODE_ASSOC);
+	$smarty->assign('exp_list_without_ts', $exp_list_without_ts);
+	$sql = rtrim(file_get_contents('sql/a14to_eta_list.sql'));
+	$sql=stritr($sql,$params);
+	$eta_list = $db->getAll($sql, null, null, null, MDB2_FETCHMODE_ASSOC);
+	$smarty->assign('eta_list', $eta_list);
+	if (isset($_REQUEST['generate']))
+	{
+		$sql=rtrim(file_get_contents('sql/a16cora.sql'));
+		$sql=stritr($sql,$params);
+		//echo $sql;
+		$d = $db->getAll($sql, null, null, null, MDB2_FETCHMODE_ASSOC);
+		foreach($d as $k=>$v)
+		{
+			$sql=rtrim(file_get_contents('sql/a16cora_photos.sql'));
+			$params[':tp_kod']=$v['tp_kod_key'];
+			$params[':dt']="'".$v['vd']."'";
+			$sql=stritr($sql,$params);
+			$d[$k]['photos'] = $db->getAll($sql, null, null, null, MDB2_FETCHMODE_ASSOC);
+		}
+		$smarty->assign('d', $d);
+	}
+	$smarty->display('a16cora.html');
+}
+?>
---
+++ sql/a16cora_photos.sql
@@ -0,0 +1,49 @@
+/* Formatted on 13.12.2016 17:50:51 (QP5 v5.252.13127.32867) */
+SELECT t.url, t.h_url, CASE WHEN url.h_url IS NOT NULL THEN 1 END coffee_url
+  FROM a16co t, a16cotp s, a16courl url
+ WHERE     t.tp_kod_key = :tp_kod
+       AND t.visitdate = TO_DATE ( :dt, 'dd.mm.yyyy')
+       AND t.url IS NOT NULL
+       AND t.visitdate = s.visitdate(+)
+       AND t.tp_kod_key = s.tp_kod(+)
+       AND CASE
+              WHEN    :ok_ts = 1
+                   OR     :ok_ts = 2
+                      AND s.visitdate IS NOT NULL
+                      AND s.ts IS NOT NULL
+                   OR :ok_ts = 3 AND t.h_url IS NOT NULL AND s.ts IS NULL
+              THEN
+                 1
+              ELSE
+                 0
+           END = 1
+       AND CASE
+              WHEN    :ok_tm = 1
+                   OR     :ok_tm = 2
+                      AND s.visitdate IS NOT NULL
+                      AND s.tm IS NOT NULL
+                   OR :ok_tm = 3 AND t.h_url IS NOT NULL AND s.tm IS NULL
+              THEN
+                 1
+              ELSE
+                 0
+           END = 1
+       AND CASE
+              WHEN    :st_ts = 1
+                   OR :st_ts = 2 AND s.visitdate IS NOT NULL AND s.ts = 1
+                   OR :st_ts = 3 AND s.visitdate IS NOT NULL AND s.ts = 2
+              THEN
+                 1
+              ELSE
+                 0
+           END = 1
+       AND CASE
+              WHEN    :st_tm = 1
+                   OR :st_tm = 2 AND s.visitdate IS NOT NULL AND s.tm = 1
+                   OR :st_tm = 3 AND s.visitdate IS NOT NULL AND s.tm = 2
+              THEN
+                 1
+              ELSE
+                 0
+           END = 1
+       AND t.h_url = url.h_url(+)
