7daa2f04fd468cc0be814859e288bdd890f572af
Denis Yakovenko <maximka1945@gmail.com>
2018-06-03 11:36:47 +0300

    Small fix in daily plans activities

--- sql/activ_plan_daily.sql
+++ sql/activ_plan_daily.sql
@@ -1,9 +1,10 @@
-/* Formatted on 09/04/2015 13:57:46 (QP5 v5.227.12220.39724) */
-  SELECT TO_CHAR (d.DATA, 'dd.mm.yyyy') data,
+/* Formatted on 03.06.2018 11:32:38 (QP5 v5.252.13127.32867) */
+  SELECT TO_CHAR (c.DATA, 'dd.mm.yyyy') data,
          d.city,
          d.PLAN,
          d.doc
-    FROM p_activ_plan_daily d
-   WHERE     d.tn = :tn
-         AND TRUNC (d.DATA, 'mm') = TO_DATE (:month_list, 'dd.mm.yyyy')
+    FROM p_activ_plan_daily d, calendar c
+   WHERE     c.data = d.data(+)
+         AND :tn = d.tn(+)
+         AND TO_DATE ( :month_list, 'dd.mm.yyyy') = TRUNC (c.data, 'mm')
 ORDER BY d.DATA
