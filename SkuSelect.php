9343d98411c79630896b69f02846956318bbc023
Taras Daragan <daragan.t.g@gmail.com>
2019-01-08 12:33:18 +0200

    Ready SkuSelect. Fix MAreport create. Added new akciya 'Naletaj, toropis'

--- SkuSelect.php
+++ SkuSelect.php
@@ -25,6 +25,10 @@ class SkuSelect
 //        }
     }
     private function controllerCreateHtml($data,$ack_type,$is_calc = false){
+        echo '<pre style="display: none;">';
+        //print_r($data);
+        echo '</pre>';
+
         $html = '<table id="sku_selected" width="100%" border="1" style="max-width: 900px;background-color: #fff;">'.
                 '<thead style="background-color: #03a9f454;">'.
                 '<tr style="font-size: 10px;font-weight: bold;text-align: center;">'.
@@ -46,7 +50,7 @@ class SkuSelect
                 $html .= '<td rowspan="2" class="type2">Объем продаж с учетом скидки, грн с НДС</td>';
             $html .= '<td rowspan="2">СС на объем отгрузки,грн с НДС</td>' .
                 '<td rowspan="2">Ожидаемая ВП,грн с НДС</td>' .
-                '<td colspan="5" class="colspan5">Расходы сети итого, вт.ч.</td>' .
+                '<td colspan="'.($ack_type == 2 ? "4" : "5").'" class="colspan5">Расходы сети итого, вт.ч.</td>' .
                 '<td rowspan="2">Чистая прибыль, грн с НДС</td>';
             if ($ack_type == 2) {
                 $html .= '<td rowspan="2" class="type2">Расходы по акции в скидке, грн с НДС</td>' .
@@ -95,13 +99,28 @@ class SkuSelect
                     $sku['price_net'] .
                     '</td>';
                 if ($ack_type == 2 && $is_calc)
-                    $html .= '<td class="type2" style="display: none;">' .
-                        $sku['price_net_disc'] .
-                        '</td>';
-                $html .= '<td>' .
-                    $sku['total_q'] .
-                    '</td>' .
-                    '</tr>';
+                    $html .= '<td class="type2" >' .$sku['price_net_disc'] .'</td>';
+                $html .= '<td>' .$sku['total_q'] .'</td>';
+                if($is_calc){
+                    $html .= '<td>'.$sku['total_volume_price'].'</td>'.
+                        '<td>'.$sku['total_volume_price_one'].'</td>';
+                    if ($ack_type == 2)
+                        $html .= '<td class="type2">'.$sku['total_volume_price_one_discount'].'</td>';
+                    $html .= '<td>'.$sku['ss_volume'].'</td>'.
+                        '<td>'.$sku['expected_vp'].'</td>'.
+                        '<td>'.$sku['bonus_net_sku'].'</td>';
+                    if ($ack_type == 1)
+                        $html .= '<td class="type1">'.$sku['akc_expenses'].'</td>';
+                    $html .= '<td>'.$sku['share_expenses'].'</td>'.
+                        '<td>'.$sku['logistics_expens_total'].'</td>'.
+                        '<td>'.$sku['company_expens_total'].'</td>'.
+                        '<td>'.$sku['net_clear'].'</td>';
+                    if ($ack_type == 2)
+                        $html .= '<td class="type2">'.$sku['prom_costs_discount'].'</td>'.
+                        '<td class="type2">'.$sku['total_network_costs'].'</td>';
+
+                }
+                $html .='</tr>';
             }
         }
         $html .='</tbody>'.
@@ -111,19 +130,72 @@ class SkuSelect
         }
         return $html;
     }
-    private function CalculateSku(&$data){
+    private function CalculateSku(&$data,$akc_type,$bonus_net_perc = null,$net_id = null,$akciya_expences_perc = null){
         if(count($data) <=0 )
             return false;
-        foreach ($data as $k => $sku){
-            foreach ($sku as $key => $item) {
-                $data[$k][$key] = $this->NormalizeFloat($item);
+        $is_calc = false;
+        try {
+            foreach ($data as $k => $sku) {
+                foreach ($sku as $key => $item) {
+                    $data[$k][$key] = $this->NormalizeFloat($item);
+                }
+                //calculate
+                if (!is_null($bonus_net_perc)) {
+                    $is_calc = true;
+                    $bonus_distr_pers = $this->modalGetDistribBonus($net_id);
 
+                    //$data[$k]['total_q'];//объем продаж шт.
+                    $data[$k]['total_volume_price'] = $data[$k]['total_q'] * $data[$k]['weight'];//объем продаж кг.
+                    $data[$k]['total_volume_price_one'] = $data[$k]['total_q'] * $data[$k]['price_net'];//объем продаж грн
+                    $data[$k]['price_net_disc'] = $data[$k]['price_net'] - (($data[$k]['price_net'] * $akciya_expences_perc) / 100);
+                    $data[$k]['total_volume_price_one_discount'] = $data[$k]['total_q'] * $data[$k]['price_net_disc'];//объем продаж грн. СКИДКА
+                    $data[$k]['ss_volume'] = $data[$k]['total_q'] * $data[$k]['price_ss'];//СС на объем отгрузки
+                    if ($akc_type == 1) {
+                        $data[$k]['expected_vp'] = $data[$k]['total_volume_price_one'] - $data[$k]['ss_volume'];//Ожидаемая ВП
+                        $data[$k]['bonus_net_sku'] = $data[$k]['total_volume_price_one'] * $bonus_net_perc;//бонус сети
+                    } else {
+                        $data[$k]['expected_vp'] = $data[$k]['total_volume_price_one_discount'] - $data[$k]['ss_volume'];//Ожидаемая ВП
+                        $data[$k]['bonus_net_sku'] = $data[$k]['total_volume_price_one_discount'] * $bonus_net_perc;//бонус сети
+                    }
+                    $data[$k]['share_expenses'] = $data[$k]['price_uk'] * $data[$k]['total_q'] * $bonus_distr_pers;//Бонус дистриб
+                    $data[$k]['akc_expenses'] = $data[$k]['total_volume_price_one'] * $akciya_expences_perc;//затраты по акции б/н
+                    $data[$k]['logistics_expens_total'] = $data[$k]['total_volume_price'] * $data[$k]['logistic_expens'];//Расходы по логистике
+// --- Расходы компании: (все расходы компании)+(маркетинг к себестоимости из плана тек. месяца)
+                    //$data[$k]['company_expens'] + $data[$k]['market_val']
+                    $data[$k]['company_expens_total'] = ($data[$k]['company_expens'] + $data[$k]['market_val']) * $data[$k]['ss_volume'];//Расходы компании
+                    $data[$k]['net_clear'] = $data[$k]['expected_vp'] - $data[$k]['logistics_expens'] - $data[$k]['company_expenses'];//чистая прибыль
+                    if ($akc_type == 1)
+                        $data[$k]['net_clear'] -= $data[$k]['akc_expenses'];//чистая прибыль
+                    $data[$k]['prom_costs_discount'] = $data[$k]['total_volume_price_one'] - $data[$k]['total_volume_price_one_discount'];//Расходы по акции в скидке
+                    $data[$k]['total_network_costs'] = $data[$k]['bonus_net_sku'] + $data[$k]['share_expenses'] + $data[$k]['logistics_expens_total'] + $data[$k]['company_expens_total'] + $data[$k]['prom_costs_discount'];//Всего затрат по сети
+                }
             }
+            return $is_calc;
+        }catch (Exception $e){
+            echo $e->getMessage();
+            return false;
         }
-        //calculate
-        return false;
 
     }
+
+    private function modalGetDistribBonus($net_id){
+        $distrBonus = 0;
+        if(!is_null($net_id) && $net_id != ""){
+            $params[":net_id"] = $net_id;
+            $sql = rtrim(file_get_contents('sql/sku_avk_distrib_bonus.sql'));
+            $sql = stritr($sql, $params);
+            $sql = trim(preg_replace('/\s+/', ' ', $sql));
+
+            $get_list = $this->db->getAll($sql, null, null, null, MDB2_FETCHMODE_ASSOC);
+            if (isset($get_list))
+            {
+                $distrBonus = $get_list['procent'];
+            }
+
+        }
+        return $distrBonus;
+    }
+
     private function NormalizeFloat($number){
         $number = $number."";
         if(substr($number,0,1) == "."){
@@ -131,13 +203,18 @@ class SkuSelect
         }
         return $number;
     }
-    public function GetSkuList($z_id,$ack_type,$print = true){
+    public function GetSkuList($z_id,$akc_type,$bonus_net_perc = null,$net_id = null,$akciya_expences_perc = null,$print = true){
         if(is_null($z_id) || $z_id == "")
             return false;
         try {
             $sku_list = $this->modalLoadSku($z_id);
-            $is_calc = $this->CalculateSku($sku_list);
-            $skuHtml = $this->controllerCreateHtml($sku_list, $ack_type, $is_calc);
+            if(!is_null($bonus_net_perc)){
+                $is_calc = $this->CalculateSku($sku_list,$akc_type,$bonus_net_perc,$net_id,$akciya_expences_perc);
+            }else{
+                $is_calc = $this->CalculateSku($sku_list,$akc_type);
+            }
+
+            $skuHtml = $this->controllerCreateHtml($sku_list, $akc_type, $is_calc);
             if ($print)
                 echo $skuHtml;
             else
--- testSkuSelect.php
+++ testSkuSelect.php
@@ -1,8 +1,8 @@
 <?php
-//include "SkuSelect.php";
-//$skuObj = new \SkuSelect\SkuSelect($db);
-//$smarty->assign('skuObj', $skuObj);
-//
-//$smarty->display('testSkuSelect.html');
+include "SkuSelect.php";
+$skuObj = new \SkuSelect\SkuSelect($db);
+$smarty->assign('skuObj', $skuObj);
+
+$smarty->display('testSkuSelect.html');
 ?>
-<link type="text/css" rel="stylesheet" href="css/portal.css?<?php echo filemtime( 'css/portal.css' )?>">
+<!--<link type="text/css" rel="stylesheet" href="css/portal.css?--><?php //echo filemtime( 'css/portal.css' )?><!--">-->
